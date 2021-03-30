e10a754f42d3ee3750c1c16826ddc3adb2d69ca5
Lematthew <74629769+Lematthew@users.noreply.github.com>
2021-03-30 11:59:30 -0700

    Update app.js
    Update with sign in with email/password

--- public/app.js
+++ public/app.js
@@ -1,27 +1,52 @@
 ///// User Authentication /////
-
 const auth = firebase.auth();
 
-const whenSignedIn = document.getElementById('whenSignedIn');
-const whenSignedOut = document.getElementById('whenSignedOut');
+//Sections to display in specific states.
+const whenSignedIn    = document.getElementById('whenSignedIn');
+const whenSignedOut   = document.getElementById('whenSignedOut');
 
-const signInBtn = document.getElementById('signInBtn');
-const signOutBtn = document.getElementById('signOutBtn');
+//Goole Login documents
+const signInBtn       = document.getElementById('signInBtn');
+const signOutBtn      = document.getElementById('signOutBtn');
+const userDetails     = document.getElementById('userDetails');
+const provider        = new firebase.auth.GoogleAuthProvider();
 
-const userDetails = document.getElementById('userDetails');
+//Signup/Login with Email and Passowrd documents
+const txtEmail        = document.getElementById('txtEmail');
+const txtPassword     = document.getElementById('txtPassword');
+const emailSignInBtn  = document.getElementById('emailSignInBtn');
+const emailSignUpBtn  = document.getElementById('emailSignUpBtn');
 
-const provider = new firebase.auth.GoogleAuthProvider();
-
-/// Sign in event handlers
-signInBtn.onclick = () => auth.signInWithPopup(provider);
+/// Sign in event handlers (Google)
+signInBtn.onclick  = () => auth.signInWithPopup(provider);
 signOutBtn.onclick = () => auth.signOut();
 
+// Email sign in 
+emailSignInBtn.addEventListener('click', e => {
+
+    const email = txtEmail.value;
+    const pass  = txtPassword.value;
+    const promise = auth.signInWithEmailAndPassword(email,pass);
+    promise.catch(e=> console.log(e.message));
+})
+
+//Email sign up
+emailSignUpBtn.addEventListener('click', e => {
+
+    const email = txtEmail.value;
+    const pass  = txtPassword.value;
+    const promise = auth.createUserWithEmailAndPassword(email,pass);
+    promise.catch(e=> console.log(e.message));
+})
+
+
+//Switch from 'Signed out' state to 'Signed in' state
 auth.onAuthStateChanged(user => {
     if (user) {
         // signed in
         whenSignedIn.hidden = false;
         whenSignedOut.hidden = true;
-        userDetails.innerHTML = `<h3>Hello ${user.displayName}!</h3> <p>User ID: ${user.uid}</p>`;
+        userDetails.innerHTML = `<h3>Hello ${user.email}!</h3> <p>User ID: ${user.uid}</p>`;
     } else {
         // not signed in
         whenSignedIn.hidden = true;
@@ -29,5 +54,6 @@ auth.onAuthStateChanged(user => {
         userDetails.innerHTML = '';
     }
 });
+
 
 
