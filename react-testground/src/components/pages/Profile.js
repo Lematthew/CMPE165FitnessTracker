98725e7613b9bc3d4190171116f247fbcf96a566
Lematthew <74629769+Lematthew@users.noreply.github.com>
2021-04-22 15:52:02 -0700

    Raymond's Branch with Firebase
    Sign-up page finished

---
+++ react-testground/src/components/pages/Profile.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../../App.css';
+import {useAuth} from '../../contexts/AuthContext'
+
+//Profile in progress
+export default function Profile() {
+    const { currentUser, logout } = useAuth()
+    if(currentUser && currentUser.email)
+
+    
+
+    return( 
+    
+    <h2>{currentUser.email}</h2>
+    
+    )
+}
---
+++ react-testground/src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/app"
+import "firebase/auth"
+
+const app = firebase.initializeApp({
+    "apiKey": "AIzaSyD1G7sOUHwg5X-AqKRrRURbuHB8G6qH-A0",
+    "authDomain": "fitness-tracker-5c549.firebaseapp.com",
+    "databaseURL": "https://fitness-tracker-5c549-default-rtdb.firebaseio.com",
+    "projectId": "fitness-tracker-5c549",
+    "storageBucket": "fitness-tracker-5c549.appspot.com",
+    "messagingSenderId": "279044668457",
+    "appId": "1:279044668457:web:62083e64045711c0caf038"
+})
+
+export const auth = app.auth()
+export default app
