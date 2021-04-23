3f585adcd7ac7464ad2303dd208d79950e317687
Raymond Chin <chin.raymond.andrew@gmail.com>
2021-04-22 20:21:14 -0700

    Profile page primitive layout; generalized Button

--- reactTestground/react-testground/src/components/Navbar.js
+++ reactTestground/react-testground/src/components/Navbar.js
@@ -62,7 +62,7 @@ function Navbar() {
                             </Link>
                         </li>           
                     </ul>
-                    {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
+                    {button && <Button buttonStyle='btn--outline' linkTo='/signup'>SIGN UP</Button>}
                 </div>
             </nav>
         </>
--- reactTestground/react-testground/src/components/pages/Profile.js
+++ reactTestground/react-testground/src/components/pages/Profile.js
@@ -1,6 +1,60 @@
 import React from 'react';
 import '../../App.css';
+import Footer from '../Footer';
+import { Button } from '../Button';
+import './Profile.css';
 
-export default function Profile() {
-    return <h1 className="profile">PROFILE</h1>
+export default function Profile(props) {
+    return (
+        <>
+            <div className="profile">
+                <h1>PROFILE</h1>
+                <div className="break" />
+
+                <div className="profile-wrapper">
+                    <h3>Welcome, username{/*USERNAME*/}</h3>
+                    <div className="break" />
+
+                    <h5>sample message{/*MESSAGE*/}</h5>
+                    <div className="break" />
+
+                    <div className="user-data">
+                        <div className="user-data-wrapper">
+                            <div className="user-data-item">
+                                <h2>Sex </h2>
+                                <p>M{/*SEX*/}</p>
+                            </div>
+                            <div className="user-data-item">
+                                <h2>Age </h2>
+                                <p>22{/*AGE*/}</p>
+                            </div>
+                            <div className="user-data-item">
+                                <h2>Height </h2>
+                                <p>5'6"{/*HEIGHT*/}</p>
+                            </div>
+                            <div className="user-data-item">
+                                <h2>Weight </h2>
+                                <p>200 lbs{/*WEIGHT*/}</p>
+                            </div>
+                            <div className="break" />
+                            <div className="user-data-item">
+                                <h2>Current Plan</h2>
+                                <p>PLAN NAME{/*PLAN*/}</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="break" />
+                    <Button className="btns" buttonStyle='btn--outline' buttonSize='btn--large' linkTo='/statistics'>
+                        Edit Profile
+                    </Button>
+
+                    <Button className="btns" buttonStyle='btn--outline' buttonSize='btn--large' linkTo='/'>
+                        Delete Profile
+                    </Button>
+                </div>
+            </div>
+            <Footer />
+        </>
+    );
 }
